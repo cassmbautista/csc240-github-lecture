586ec4ff8b0591c87d562f592552983c762137a1
Cassandra Bautista <cass.m.bautista@gmail.com>
2026-09-22 17:47:19 -0700

    add test program

---
+++ test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void addScore(std::vector<int> scores) {
+    scores.push_back(100);
+}
+
+void swapScores(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main() {
+    int score = 10;
+    int currentScore = score;
+
+    cout << score << endl;
+    cout << currentScore << endl;
+    cout << &score << endl;
+    cout << &currentScore << endl;    
+}
